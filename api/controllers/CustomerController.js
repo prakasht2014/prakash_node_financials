9c98dec763f4212c88a7d0d8484366bb7d1bb76e
Prakash Thangavel <prakasht2014@gmail.com>
2016-07-08 02:27:34 +0530

    added destroy function to customer controller

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -49,6 +49,11 @@ module.exports = {
 			}
 			res.redirect('/customer/show/'+req.param('id'));
 		});
+	},
+	destroy:function(req,res,next){
+		Customer.destroy(req.param('id')).exec(function(){
+			res.redirect('/customer/');
+		});
 	}
 };
 
